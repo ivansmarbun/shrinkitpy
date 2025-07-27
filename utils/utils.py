ad4b576f00dc25e4723385caf91bca9efda3662f
ivansmarbun <ivansmarbun@gmail.com>
2025-07-27 18:59:48 +0700

    fix formatting

--- utils/utils.py
+++ utils/utils.py
@@ -3,6 +3,4 @@ import random
 
 
 def generate_short_id(num_chars=6):
-    return "".join(
-        random.choices(string.ascii_letters + string.digits, k=num_chars)
-    )
+    return "".join(random.choices(string.ascii_letters + string.digits, k=num_chars))
